d2e423c63a0d1943412a180c9e04db80358690ff
bacsiemon <johnsieunhan816@Gmail.com>
2024-02-26 19:17:56 +0700

    Added some lombok annotations

--- birthdaybooking/src/main/java/com/swp/birthdaybooking/entities/Schedule.java
+++ birthdaybooking/src/main/java/com/swp/birthdaybooking/entities/Schedule.java
@@ -1,7 +1,9 @@
 package com.swp.birthdaybooking.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,18 +11,21 @@ import lombok.Setter;
 
 import java.util.Date;
 
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
+@Table(name = "Schedule")
 @Getter
 @Setter
-@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Schedule {
 
     @Id
+    @Column(name = "Schedule_id")
     private int scheduleId;
 
+    @Column(name = "date")
     private Date date;
 
+    @Column(name = "busy")
     private boolean busy;
-
 }
